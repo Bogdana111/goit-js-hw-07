431f91fa5be2a0f98293ed7f1ce09a812df58e8b
Bogdana111 <124085296+Bogdana111@users.noreply.github.com>
2023-06-11 04:03:55 +0200

    add tasks 2 and 3

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   "Herbs",
   "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+
+const items = ingredients.map((el) => {
+  const itemEl = document.createElement("li");
+
+  itemEl.classList.add("item");
+
+  itemEl.textContent = el;
+
+  return itemEl;
+});
+
+list.append(...items);
+
+console.log(list);
