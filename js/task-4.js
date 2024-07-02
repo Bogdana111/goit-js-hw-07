802e1b1d43036af05ac5c4163e072032f50ce7bf
Bohdana <LipskaBogdana1990@gmail.com>
2024-07-02 17:51:10 +0200

    corrected task-4

--- js/task-4.js
+++ js/task-4.js
@@ -9,14 +9,13 @@ function handleSabmit(event) {
   const emailValue = form.elements.email.value.trim();
   const passwordValue = form.elements.password.value.trim();
 
-  if (emailValue === "" && passwordValue === "") {
+  if (emailValue === "" || passwordValue === "") {
     alert(`All form fields must be filled in`);
+  } else if (emailValue !== "" || passwordValue !== "") {
+    info.email = emailValue;
+    info.password = passwordValue;
+    console.log(info);
   }
-
-  info.email = emailValue;
-  info.password = passwordValue;
-
-  console.log(info);
 
   form.reset();
 }
